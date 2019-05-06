48e05786f9c77a8dd06c6dea7d10d03e6f66e6c0
achellko <biara.prime@gmail.com>
2019-05-06 08:55:02 +0200

    rob
    draw robot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Konva from "konva";
 import { render } from "react-dom";
-import { Stage, Layer, Shape, Circle } from "react-konva";
+import { Stage, Layer, Shape, Circle, Rect } from "react-konva";
 //import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
 class App extends Component {
@@ -24,22 +24,66 @@ class App extends Component {
             stroke="black"
             strokeWidth={4}
           />
-          <Circle x={440} y={280} radius={15} fill="blue" />
-          <Circle x={360} y={280} radius={15} fill="blue" />
-          {/* { <Shape
+          <Shape
             sceneFunc={(context, shape) => {
               context.beginPath();
-              context.moveTo(450, 300);
-              context.lineTo(220, 80);
-              context.quadraticCurveTo(150, 100, 260, 170);
+              context.moveTo(500, 300);
+              context.lineTo(600, 400);
+              context.lineTo(500, 400);
               context.closePath();
               // (!) Konva specific method, it is very important
               context.fillStrokeShape(shape);
             }}
-            fill="red"
+            fill="#00D2FF"
             stroke="black"
-            strokeWidth={1}
-          /> } */}
+            strokeWidth={3}
+          />
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(300, 300);
+              context.lineTo(200, 400);
+              context.lineTo(300, 400);
+              context.closePath();
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="black"
+            strokeWidth={3}
+          />
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(500, 500);
+              context.lineTo(500, 600);
+              context.lineTo(450, 600);
+              context.lineTo(450, 500);
+              context.closePath();
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="black"
+            strokeWidth={3}
+          />
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(300, 500);
+              context.lineTo(300, 600);
+              context.lineTo(350, 600);
+              context.lineTo(350, 500);
+              context.closePath();
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="black"
+            strokeWidth={3}
+          />
+          <Circle x={440} y={280} radius={15} fill="blue" />
+          <Circle x={360} y={280} radius={15} fill="blue" />
         </Layer>
       </Stage>
     );
